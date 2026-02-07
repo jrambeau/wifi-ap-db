5dbd2ab41f08f2b2831d0866929d6bd8d9d73d9c
jrambeau <jonathan.rambeau@axians.com>
2026-02-07 09:45:50 +0100

    new build

--- src/components/ui/InfoModal.tsx
+++ src/components/ui/InfoModal.tsx
@@ -14,7 +14,7 @@ export default function InfoModal({ onClose }: InfoModalProps) {
   const staticBuildDate = new Date(__BUILD_TIMESTAMP__);
   
   const buildInfo = {
-    version: '1.3.6',
+    version: '1.3.7',
     // Static build date & time (when npm run build was executed)
     buildDateTime: staticBuildDate.toLocaleString('en-US', {
       year: 'numeric',
